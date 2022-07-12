7b9be8046342335ddc5ab50b7bd74dd61edc8af5
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-07-12 18:08:54 +0200

    Initial commit

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,11 @@
+package application;
+
+import java.lang.reflect.Array;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println(Arrayz.firstLast6());
+        System.out.println(Arrayz.sameFirstLast());
+    }
+}
