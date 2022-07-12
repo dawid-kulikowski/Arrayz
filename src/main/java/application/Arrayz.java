639dcedb99b9f2bd39be43b5e69b50c8ab8a51ae
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-07-12 19:00:54 +0200

    Array - 1 > maxEnd3

--- src/main/java/application/Arrayz.java
+++ src/main/java/application/Arrayz.java
@@ -94,4 +94,21 @@ public class Arrayz {
         }
     }
 
+
+//    Given an array of ints, return the sum of the first 2 elements in the array. If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+//    sum2([1, 2, 3]) → 3
+//    sum2([1, 1]) → 2
+//    sum2([1, 1, 1, 1]) → 2
+
+    public int sum2() {
+        int[] nums = {2,3,4,5,6,7};
+        if (nums.length==0) {
+            return 0;
+        } else if (nums.length<2) {
+            return nums[0];
+        } else{
+            return nums[0]+nums[1];
+        }
+    }
+
 }
