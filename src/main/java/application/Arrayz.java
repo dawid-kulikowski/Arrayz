afb030f90fa7dc5738f38c1d70f1dbd2ab744d24
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-07-12 18:17:01 +0200

    Array - 1 > makePi

--- src/main/java/application/Arrayz.java
+++ src/main/java/application/Arrayz.java
@@ -16,7 +16,6 @@ public class Arrayz {
 
 
 //    Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
-//
 //        sameFirstLast([1, 2, 3]) → false
 //    sameFirstLast([1, 2, 3, 1]) → true
 //    sameFirstLast([1, 2, 1]) → true
@@ -30,7 +29,16 @@ public class Arrayz {
         else {
             return false;
         }
-
     }
+
+
+//    Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+//    makePi() → [3, 1, 4]
+    public static int[] makePi() {
+    return new int[] {3,1,4};
+}
+
+
+
 
     }
