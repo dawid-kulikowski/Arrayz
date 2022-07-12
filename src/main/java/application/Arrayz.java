2e200e03f501387a08056f73c9ef3374f5acf5dd
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-07-12 18:45:13 +0200

    Array - 1 > rotateLeft3, reverse3

--- src/main/java/application/Arrayz.java
+++ src/main/java/application/Arrayz.java
@@ -60,8 +60,23 @@ public class Arrayz {
         return nums[0]+nums[1]+nums[2];
     }
 
+//    Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+//    rotateLeft3([1, 2, 3]) → [2, 3, 1]
+//    rotateLeft3([5, 11, 9]) → [11, 9, 5]
+//    rotateLeft3([7, 0, 0]) → [0, 0, 7]
 
 
+    public static int[] rotateLeft3() {
+        int[] nums = {1,2,3};
+        return new int[] {nums[1],nums[2],nums[0]};
+    }
+
+//ś
+
+    public static int[] reverse3() {
+        int[] nums = {1,2,3};
+        return new int[] {nums[2],nums[1],nums[0]};
+    }
 
 
 }
