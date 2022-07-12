5ee307b3a8b238716356e8a3c294b36e171f7a64
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-07-12 19:39:56 +0200

    Array - 1 > sum2, middleWay, makeEnds

--- src/main/java/application/Arrayz.java
+++ src/main/java/application/Arrayz.java
@@ -111,4 +111,27 @@ public class Arrayz {
         }
     }
 
+//    Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+//    middleWay([1, 2, 3], [4, 5, 6]) → [2, 5]
+//    middleWay([7, 7, 7], [3, 8, 0]) → [7, 8]
+//    middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
+
+
+    public int[] middleWay() {
+        int[] a = {1,2,3};
+        int[] b = {3,4,5};
+        return new int[] {a[1],b[1]};
+    }
+
+//    Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
+//    makeEnds([1, 2, 3]) → [1, 3]
+//    makeEnds([1, 2, 3, 4]) → [1, 4]
+//    makeEnds([7, 4, 6, 2]) → [7, 2]
+
+    public int[] makeEnds() {
+        int[] nums = {1,2,3,4,5};
+        return new int[] {nums[0],nums[nums.length-1]};
+    }
+
+
 }
